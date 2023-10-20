601db0bbac51247a7a0b899821391d09b5adb205
Gaston Herrlein <herrleingaston@gmail.com>
2023-10-20 12:10:06 +0200

    feature: 'HumanPlayer' class is added, and the 'play' method is refactored.

--- player.py
+++ player.py
@@ -1,20 +1,52 @@
 from oracle import BaseOracle, ColumnRecommendation
-from settings import ColumnClassification
+from square_board import SquareBoard
+
+from settings import ColumnClassification, BOARD_LENGTH
+
+def _is_within_column_range (col):
+    return (col>=0 and col < BOARD_LENGTH) 
+    
+def _is_non_full_column (board, col):
+    return not board._columns[col].is_full()
+
+def _is_int (col):
+    try:
+        nume = int(col)
+        return True
+    except:
+        return False 
 
 class Player ():
-
     def __init__(self, name, char, oracle = BaseOracle()) -> None:
         self.name = name
         self.char = char
         self._oracle = oracle
 
     def play (self, board):
-        recommendation = self._oracle.get_recommendation (board, self)
+        (best, recommendation) = self._ask_oracle (board)
+        self._play_on (board, best.index)
+    
+    def _play_on (self, board, index):
+        board.add (self.char, index)
+
+    def _ask_oracle (self, board):
+        recommendation = self._oracle.get_recommendation(board, self)
         best = self._choose(recommendation)
-        board.add (self.char, best.index)
+        return (best, recommendation)
 
     def _choose (self, recommendations):
         valid = list(filter(lambda x: x.classification != ColumnClassification.FULL, recommendations))
         return valid[0]
 
-        
+class HumanPlayer (Player):
+    def __init__ (self, name, char):
+        super.__init__(name, char)
+    
+    def _ask_oracle (self, board):
+        while True:
+            raw = input ("Selecciona una columna: ")
+            validation = _is_int(raw) and _is_within_column_range (int(raw)) and _is_non_full_column (int(raw))
+            if validation:
+                pos = int(raw)
+                return (ColumnRecommendation(pos, None), None)
+            
--- player_test.py
+++ player_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from square_board import SquareBoard
 from oracle import BaseOracle
-from player import Player
+from player import Player, _is_non_full_column, _is_int, _is_within_column_range
 
 
 def test_play ():
@@ -20,3 +20,37 @@ def test_play ():
     player.play(before)
 
     assert before == after
+
+def test_valid_column():
+    board = SquareBoard.fromList([['x', None, None, None, ],
+                                  ['x', 'o', 'x', 'o', ],
+                                  ['o', 'o', 'x', 'x', ],
+                                  ['o', None, None, None, ]])
+
+    assert _is_within_column_range(0)
+    assert _is_within_column_range(1)
+    assert _is_within_column_range(2)
+    assert _is_within_column_range(3)
+    assert _is_within_column_range(5) == False
+    assert _is_within_column_range(-10) == False
+    assert _is_within_column_range(10) == False
+
+def test_is_non_full_column():
+    board = SquareBoard.fromList([['x', None, None, None, ],
+                                  ['x', 'o', 'x', 'o', ],
+                                  ['o', 'o', 'x', 'x', ],
+                                  ['o', None, None, None, ]])
+                                
+    assert _is_non_full_column(board, 0)
+    assert _is_non_full_column(board, 1) == False
+    assert _is_non_full_column(board, 2) == False
+    assert _is_non_full_column(board, 3) 
+
+def test_is_int():
+    assert _is_int('42')
+    assert _is_int('0')
+    assert _is_int('-32')
+    assert _is_int('  32   ')
+    assert _is_int('hola') == False
+    assert _is_int('') == False
+    assert _is_int('3.14') == False
